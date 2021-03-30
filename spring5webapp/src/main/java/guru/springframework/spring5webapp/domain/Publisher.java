42998d44ca27c9080b6a55f5aa778f99b5c77c04
dzastinka <jslawska5@gmail.com>
2021-03-30 20:04:43 +0200

    add publisher relationship

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -7,7 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -23,6 +27,10 @@ private String addressLine1;
 private String city;
 private String state;
 private String zip;
+
+@OneToMany
+@JoinColumn(name = "publisher_id")
+private Set<Book> books = new HashSet<>();
 
 public Publisher() {
 }
